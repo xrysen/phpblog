fb793efc333138f959b5c5a7d1b43290d11286c6
Sean Oyler <xrysen@gmail.com>
2021-06-23 12:48:17 -0700

    Sets up root path

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  session_start();
+
+  define ('ROOT_PATH', realpath(dirname(__FILE__)));
+  define ('BASE_URL', 'http://127.0.0.1/edsa-PHP%20Blog/');
+
+?>
--- index.php
+++ index.php
@@ -1,25 +1,28 @@
-<?php require_once('includes/header.php'); ?>
-	<title>LifeBlog | Home </title>
+<?php require_once("config.php"); ?>
+<?php require_once(ROOT_PATH . '/includes/header.php'); ?>
+<title>LifeBlog | Home </title>
 </head>
+
 <body>
-	<!-- container - wraps whole page -->
-	<div class="container">
-		<?php require_once('includes/navbar.php'); ?>
-    <?php require_once('includes/banner.php'); ?>
+  <!-- container - wraps whole page -->
+  <div class="container">
+    <?php require_once(ROOT_PATH . '/includes/navbar.php'); ?>
+    <?php require_once(ROOT_PATH . '/includes/banner.php'); ?>
 
-		<!-- Page content -->
-		<div class="content">
-			<h2 class="content-title">Recent Articles</h2>
-			<hr>
-			<!-- more content still to come here ... -->
-		</div>
-		<!-- // Page content -->
+    <!-- Page content -->
+    <div class="content">
+      <h2 class="content-title">Recent Articles</h2>
+      <hr>
+      <!-- more content still to come here ... -->
+    </div>
+    <!-- // Page content -->
 
-		<!-- footer -->
-		<?php require_once('includes/footer.php'); ?>
-		<!-- // footer -->
+    <!-- footer -->
+    <?php require_once(ROOT_PATH . '/footer.php'); ?>
+    <!-- // footer -->
 
-	</div>
-	<!-- // container -->
+  </div>
+  <!-- // container -->
 </body>
+
 </html>
